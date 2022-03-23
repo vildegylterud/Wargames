03b1f7f321176c6d56430274c1cb574345397662
Vilde Gylterud <vildegylterud@outlook.com>
2022-03-23 18:55:50 +0100

    JavaFX NullPointerException thrown in Main.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module Wargames {
+
+    requires javafx.controls;
+    requires javafx.fxml;
+    exports no.ntnu.vildegy.wargames.io.Battle;
+    exports no.ntnu.vildegy.wargames.io.Units;
+    exports no.ntnu.vildegy.wargames.filehandler;
+    exports no.ntnu.vildegy.wargames.frontend;
+
+
+}
---
+++ src/main/java/no/ntnu/vildegy/wargames/frontend/MainController.java
@@ -0,0 +1,154 @@
+package no.ntnu.vildegy.wargames.frontend;
+
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import no.ntnu.vildegy.wargames.filehandler.ExportUnits;
+import no.ntnu.vildegy.wargames.filehandler.ImportUnits;
+import no.ntnu.vildegy.wargames.io.Battle.Army;
+import no.ntnu.vildegy.wargames.io.Units.Unit;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class MainController {
+
+    private Army army;
+
+    @FXML
+    private ObservableList<Unit> observableList;
+
+    public TableColumn<Unit,String> armyOne;
+    public TableColumn<Unit,String> armyTwo;
+    public TableColumn<Unit,String> armyWinner;
+    public TableView<Unit> tableView;
+
+    public Button clearButton;
+    public Button simulateButton;
+
+    public AnchorPane anchorPane;
+    public MenuBar menuBar;
+
+
+    /**
+     * Initializing the application
+     * creates an instance of postal code register and an
+     * observable list to show in tableview
+     */
+    @FXML
+    public void initialize() throws IOException {
+        this.army = new Army();
+
+        ImportUnits.importArmy(String.valueOf(army));
+
+        armyOne.setCellValueFactory(new PropertyValueFactory<>("ARMY_ONE"));
+        armyTwo.setCellValueFactory(new PropertyValueFactory<>("ARMY_TWO"));
+        armyWinner.setCellValueFactory(new PropertyValueFactory<>("WINNER"));
+
+        anchorPane.setStyle("-fx-background-color: #424242");
+
+        this.observableList = FXCollections.observableArrayList();
+        updateTableView(army.getAllUnits());
+
+    }
+
+    /**
+     * Shows an about box with information of the program
+     */
+    @FXML
+    public void showAboutDialog() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Information Dialog - About");
+        alert.setContentText("Vilde Gylterud" + "\n" + "Version: 1.0-SNAPSHOT" );
+
+        //To get the dark mode design on all the dialog boxes
+        DialogPane dialogPane = alert.getDialogPane();
+        dialogPane.setStyle("-fx-background-color: #424242; -fx-prompt-text-fill: white; -fx-base: #121212; -fx-focus-traversable: false;  -fx-control-inner-background: derive(-fx-base, 35%);\n" +
+                "    -fx-control-inner-background-alt: -fx-control-inner-background ; -fx-accent: #212121;\n" +
+                "    -fx-focus-color: -fx-accent;");
+
+        alert.showAndWait();
+    }
+
+
+    /**
+     * Method for exit the application
+     * Asks the user if she really wants to exit the application
+     */
+    @FXML
+    public void exitApplication() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirm");
+        alert.setHeaderText("Exit?");
+        alert.setContentText("Are you sure you want to exit?");
+
+        //To get the dark mode design on the dialog boxes too
+        DialogPane dialogPane = alert.getDialogPane();
+        dialogPane.setStyle("-fx-background-color: #424242; -fx-prompt-text-fill: white; -fx-base: #121212; -fx-focus-traversable: false;  -fx-control-inner-background: derive(-fx-base, 35%);\n" +
+                "    -fx-control-inner-background-alt: -fx-control-inner-background ; -fx-accent: #212121;\n" +
+                "    -fx-focus-color: -fx-accent;");
+
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.isPresent() && (result.get() == ButtonType.OK)) {
+
+            Platform.exit();
+        }
+    }
+
+    /** Method that sends a selected message to the user
+     *
+     * @param message selected message to the user
+     */
+    public void messageAlertToUser(String message) {
+        Alert alert = new Alert((Alert.AlertType.INFORMATION));
+
+        alert.setTitle("Information");
+        alert.setContentText(message);
+
+        //To get the dark mode design on all the dialog boxes
+        DialogPane dialogPane = alert.getDialogPane();
+        dialogPane.setStyle("-fx-background-color: #424242; -fx-prompt-text-fill: white; -fx-base: #121212; -fx-focus-traversable: false;  -fx-control-inner-background: derive(-fx-base, 35%);\n" +
+                "    -fx-control-inner-background-alt: -fx-control-inner-background ; -fx-accent: #212121;\n" +
+                "    -fx-focus-color: -fx-accent;");
+
+        alert.showAndWait();
+    }
+
+    /** Method that clears the tableview
+     *  and sets/adds the new values in it
+     *
+     * @param arrayList a list with all the postal codes
+     */
+    public void updateTableView(ArrayList<Unit> arrayList) {
+        tableView.getItems().clear();
+        observableList.setAll(arrayList);
+        tableView.getItems().addAll(observableList);
+    }
+
+    /**
+     * Method for exporting Txt file
+     */
+    @FXML
+    public void exportToFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter("Csv files (.csv)", ".csv"));
+        File importFile = fileChooser.showSaveDialog(null);
+
+        try {
+            ExportUnits.exportToFile(this.army, importFile);
+            updateTableView(army.getAllUnits());
+        } catch (IOException e) {
+            messageAlertToUser("Error in exporting file. Could not import file");
+            e.printStackTrace();
+        }
+    }
+
+}
